8404063fa794c623ee9e4e96d4027579aa18f11f
Romain <52313983+Starmania@users.noreply.github.com>
2022-01-11 22:53:13 +0100

    Update uninstall scritp + Install script ok ?

--- expressvpn/tools/chocolateyInstall.ps1
+++ expressvpn/tools/chocolateyInstall.ps1
@@ -3,10 +3,10 @@
 $packageArgs = @{
   packageName            = 'expressvpn'
   fileType               = 'exe'
-  url64bit               = 'https://www.expressvpn.works/clients/windows/expressvpn_windows_10.14.0.8_release.exe'
-  checksum64             = 'f333857c9a2fa85e38d82ad2ad5b4cd9ba4e384e851d85fb1e8e75470c758588'
+  url64bit               = 'https://www.expressvpn.works/clients/windows/expressvpn_windows_10.15.0.8_release.exe'
+  checksum64             = 'd404b92d7635b0d0b7c486262392dd65fcb4ba7d6d77598441b15714684ab086'
   checksumType64         = 'sha256'
-  silentArgs             = '/qn'
+  silentArgs             = '/quiet'
   validExitCodes         = @(0)
   softwareName           = 'expressvpn*'
 }
