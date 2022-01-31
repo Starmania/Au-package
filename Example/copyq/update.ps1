d525a00c589d0cbacdc4e0244e75e487a7e66746
Romain <52313983+Starmania@users.noreply.github.com>
2022-01-31 22:09:33 +0100

    Remove ? and % from Powershell script examples for better reading

--- Example/copyq/update.ps1
+++ Example/copyq/update.ps1
@@ -29,10 +29,10 @@ function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases
 
     $re  = "copyq-.*-setup.exe"
-    $url = $download_page.links | ? href -match $re | select -First 1 -expand href
+    $url = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
     $url = 'https://github.com' + $url
 
-    $version = $url -split '-|.exe' | select -Last 1 -Skip 2
+    $version = $url -split '-|.exe' | Select-Object -Last 1 -Skip 2
 
     return @{
         URL32        = $url
--- Example/librecad/update.ps1
+++ Example/librecad/update.ps1
@@ -20,9 +20,9 @@ function global:au_GetLatest {
   $download_page = Invoke-WebRequest -UseBasicParsing -Uri $releases
 
   $re    = '\.exe$'
-  $url   = $download_page.links | ? href -match $re | select -First 1 -expand href
+  $url   = $download_page.links | Where-Object href -match $re | Select-Object -First 1 -expand href
 
-  $version  = ($url -split '/' | select -Last 1 -Skip 1)
+  $version  = ($url -split '/' | Select-Object -Last 1 -Skip 1)
 
   $releaseNotesUrl = "$domain/LibreCAD/LibreCAD/releases/tag/" + $version
 
