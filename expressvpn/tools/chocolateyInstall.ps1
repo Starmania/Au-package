d0a1ed99e47584a80924cfd37a69ec955f43fcf7
Romain <52313983+Starmania@users.noreply.github.com>
2022-02-01 09:37:12 +0100

    Adding explications to the -2147023258 return code
    [skip ci]

--- expressvpn/tools/chocolateyInstall.ps1
+++ expressvpn/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   checksum64           = '2d0443fff1429d44f61d92646a71a40cb9d599a4e1cd02018a34aeb98ecb9785'
   checksumType64       = 'sha256'
   silentArgs           = '/quiet'
-  validExitCodes       = @(0,-2147023258)
+  validExitCodes       = @(0,-2147023258) #The -2147023258 code is if app is already installed with the same version.
   softwareName         = 'expressvpn*'
 }
 Install-ChocolateyPackage @packageArgs
