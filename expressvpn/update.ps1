54c76d4d151cb5cde9ed53dd13110e58886d0de8
Romain <52313983+Starmania@users.noreply.github.com>
2022-04-25 22:54:35 +0200

    Update Updater, works ! and add .vscode to gitignore

--- expressvpn/update.ps1
+++ expressvpn/update.ps1
@@ -3,7 +3,7 @@ import-module au
 $releases = 'https://www.expressvpn.com/latest#windows'
 
 function global:au_SearchReplace {
-   @{
+    @{
         ".\tools\chocolateyInstall.ps1" = @{
             "(?i)(^\s*url64bit\s*=\s*)('.*')"   = "`$1'$($Latest.URL64)'"
             "(?i)(^\s*checksum64\s*=\s*)('.*')" = "`$1'$($Latest.Checksum64)'"
@@ -14,9 +14,9 @@ function global:au_SearchReplace {
 function global:au_GetLatest {
     $download_page = Invoke-WebRequest -Uri $releases -MaximumRedirection 0 -UseBasicParsing -UserAgent "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
 
-    $url64   = $download_page.Links | Where-Object href -Match '\.exe$' | ForEach-Object href | Select-Object -First 1
-    $version = $url64 -split '/' | Select-Object -Last 1
-    $version = $version -split '_' | Select-Object -Last 1 -Skip 1
+    $url64 = $download_page.Links | Where-Object href -Match '\.exe$' | ForEach-Object href | Select-Object -First 1
+    $global:version = $url64 -split '/' | Select-Object -Last 1
+    $global:version = $version -split '_' | Select-Object -Last 1 -Skip 1
 
     @{
         URL64   = $url64
@@ -24,4 +24,10 @@ function global:au_GetLatest {
     }
 }
 
-update -ChecksumFor 64
+Update-Package -ChecksumFor 64 -NoCheckUrl
+
+$fileName = 'expressvpn.' + $global:version + '.nupkg'
+
+choco push $fileName
+
+Remove-Item $fileName
