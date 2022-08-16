6ed2ea01fbf45676974534293ddbc0b2bb3ff7ba
Romain <52313983+Starmania@users.noreply.github.com>
2022-08-16 19:17:41 +0200

    Added full edition and beta edition of expressVPN

---
+++ expressvpn-full/tools/chocolateyUninstall.ps1
@@ -0,0 +1,23 @@
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'expressvpn'
+
+$uninstalled = $false
+[string]$key = Get-UninstallRegistryKey -SoftwareName 'ExpressVPN' | ForEach-Object BundleCachePath
+
+Write-Verbose ("Uninstallation: " + $key)
+
+if ($null -ne $key) {
+
+    $packageArgs = @{
+      packageName    = $packageName
+      fileType       = 'EXE'
+      silentArgs     = '/uninstall /quiet'
+      validExitCodes = @(0)
+      file           = $key
+    }
+  
+    Uninstall-ChocolateyPackage @packageArgs
+} else {
+  Write-Warning "$packageName has already been uninstalled by other means."
+}
