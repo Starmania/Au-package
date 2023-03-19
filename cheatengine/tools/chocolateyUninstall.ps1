726651a81245e798b5b9f7547d6bbc62ff983c53
Romain <52313983+Starmania@users.noreply.github.com>
2023-03-19 14:09:24 +0000

    Updated CheatEngine to 7.5 and the updater.

--- cheatengine/tools/chocolateyUninstall.ps1
+++ cheatengine/tools/chocolateyUninstall.ps1
@@ -2,4 +2,4 @@
   -PackageName 'cheatengine' `
   -FileType 'EXE' `
   -Silent '/VERYSILENT /NORESTART' `
-  -File (Get-UninstallRegistryKey -SoftwareName 'Cheat Engine 7.4').UninstallString.Trim('"')
+  -File (Get-UninstallRegistryKey -SoftwareName 'Cheat Engine 7.5').UninstallString.Trim('"')
--- cheatengine/update.py
+++ cheatengine/update.py
@@ -5,13 +5,10 @@ import subprocess
 
 from packaging import version
 import requests
+# bar
+from alive_progress import alive_bar
 
-DEBUG = True
-
-if DEBUG:
-    import time
-
-REGEX_URL = r"(https://d2oq4dwfbh6gxl.cloudfront.net/f/CheatEngine/(?:(?:[\w.~-]*|%[\da-f]{2})/)+CheatEngine\d{2}.exe)\x02\x1c\x04\x01\x14\x13([/A-Z0-9 ]+)"
+REGEX_URL = r"(https://d2oq4dwfbh6gxl.cloudfront.net/f/CheatEngine/(?:(?:[\w.~-]*|%[\da-f]{2})/)+CheatEngine\d{2}.exe)\x02....\x13([/A-Z0-9 ]+)"
 REGEX_VERSION = r"<version>(\d.\d)</version>"
 REGEX_SCRIPT = {
     "url": r"-Url '(.*)'",
@@ -25,11 +22,14 @@ REGEX_SCRIPT = {
 }
 
 
-def get_content(filename: str) -> str:
-    if DEBUG:
-        print("DEBUG: Decompiling the installer...")
+def get_content(filename: str, bar) -> str:
+    bar()
+    print("Decompiling the installer...")
     basename = ".".join(filename.split(".")[0:-1])
-    subprocess.run(["utils\innounp.exe", "-x", "-m", f"-d{basename}", "-q", "-b", "-y", f"{filename}"])
+    subprocess.run(
+        ["utils\innounp.exe", "-x", "-m", f"-d{basename}",
+         "-q", "-b", "-y", f"{filename}"]
+    )
     if os.path.exists(f"{basename}\\{{app}}"):
         return "Original"
     with open(f"{basename}\\embedded\\CompiledCode.bin", "rb") as f:
@@ -39,104 +39,125 @@ def get_content(filename: str) -> str:
     return content
 
 
-def get_real_file(version: str, filename: str):
-    version_short = version.replace(".", "")
+def get_real_file(new_version: str, filename: str, bar):
+    new_version_short = new_version.replace(".", "")
     basename = ".".join(filename.split(".")[0:-1])
-    if DEBUG:
-        print("DEBUG: Downloading the installer...")
+    bar()
+    print("Downloading the installer...")
     if not os.path.exists(f"tmp\{filename}"):
-        base_URL = f"https://github.com/cheat-engine/cheat-engine/releases/download/{version}/CheatEngine{version_short}.exe"
+        base_URL = f"https://github.com/cheat-engine/cheat-engine/releases/download/{new_version}/CheatEngine{new_version_short}.exe"
         out = requests.get(base_URL, stream=True).content
-        if DEBUG:
-            print("DEBUG: Downloaded the installer.")
+        bar()
+        print("Downloaded the installer.")
         with open(f"tmp\{filename}", "wb") as f:
             f.write(out)
-    elif DEBUG:
-        print("DEBUG: Installer already exists.")
+    else:
+        bar()
+        print("Installer already exists.")
 
-    content = get_content(f"tmp\{filename}")
+    content = get_content(f"tmp\{filename}", bar)
     if os.path.exists(f"tmp\\{basename}\\{{app}}"):
         result = {
-            "url": f"https://github.com/cheat-engine/cheat-engine/releases/download/{version}/CheatEngine{version_short}.exe",
-            "silent_args": "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /CLOSEAPPLICATIONS /NOCANCEL /DIR=\"C:\\\\Program Files\\\\Cheat Engine {0}\"".format(version),
+            "url": f"https://github.com/cheat-engine/cheat-engine/releases/download/{new_version}/CheatEngine{new_version_short}.exe",
+            "silent_args": "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /CLOSEAPPLICATIONS /NOCANCEL /DIR=\"C:\\\\Program Files\\\\Cheat Engine {0}\"".format(new_version),
         }
-        if not os.path.exists(f"tmp\CheatEngine{version_short}-real.exe"):
-            shutil.copyfile(f"tmp\CheatEngine{version_short}.exe", f"tmp\CheatEngine{version_short}-real.exe")
+        if not os.path.exists(f"tmp\CheatEngine{new_version_short}-real.exe"):
+            shutil.copyfile(f"tmp\CheatEngine{new_version_short}.exe", f"tmp\CheatEngine{new_version_short}-real.exe")
         shutil.rmtree(f"tmp\\{basename}")
+        bar(2)
         return result
     content = content.replace("\x00", "")
 
     url = re.search(REGEX_URL, content)
 
-    if url:
-        if DEBUG:
-            print("DEBUG: Found match. Downloading the real file...")
-        if not os.path.exists(f"tmp\{basename}-real.exe"):
-            file_content = requests.get(url.group(1), stream=True).content
-            if DEBUG:
-                print("DEBUG: Downloaded the real file.")
+    if not url:
+        raise ValueError(f"Could not find the URL for version {new_version_short}.")
+    
+    bar()
+    print("Found match. Downloading the real file...")
+    if not os.path.exists(f"tmp\{basename}-real.exe"):
+        file_content = requests.get(url.group(1), stream=True).content
+        bar()
+        print("Downloaded the real file.")
 
-            with open(f"tmp\{basename}-real.exe", "wb") as f:
-                f.write(file_content)
-        elif DEBUG:
-            print("DEBUG: Real file already exists.")
-        result = {
-            "url": url.group(1),
-            "silent_args": url.group(2),
-        }
-        return result
-    return None
+        with open(f"tmp\{basename}-real.exe", "wb") as f:
+            f.write(file_content)
+    else:
+        bar()
+        print("Real file already exists.")
+    result = {
+        "url": url.group(1),
+        "silent_args": url.group(2),
+    }
+    return result
 
 
 def main(new_version: str, force: bool = False):
-    if DEBUG:
-        print("DEBUG: Starting the script...")
-        # time.sleep(2)
-    with open("cheatengine.nuspec", "r+", encoding="utf-8") as file:
-        lines = file.readlines()
-        for i in range(len(lines)):
-            line = lines[i]
-            result = re.search(REGEX_VERSION, line)
-            if result:
-                old_version = version.parse(result.group(1))
-                if version.parse(new_version) > old_version or force:
-                    lines[i] = line.replace(result.group(1), new_version)
-                    break
-                return
+    with alive_bar(7, title=f"Updating to {new_version}...") as bar:
+        with open("cheatengine.nuspec", "r+", encoding="utf-8") as file:
+            lines = file.readlines()
+            for i in range(len(lines)):
+                line = lines[i]
+                result = re.search(REGEX_VERSION, line)
+                if result:
+                    old_version = result.group(1)
+                    if version.parse(new_version) > version.parse(old_version) or force:
+                        lines[i] = line.replace(old_version, new_version)
+                        break
+                    bar(7)
+                    return
 
-        file.seek(0)
-        file.truncate()
-        file.writelines(lines)
+            file.seek(0)
+            file.truncate()
+            file.writelines(lines)
 
-    if DEBUG:
-        print("DEBUG: Updating the script...")
+        bar()
+        print("Updating the script...")
 
-    version_short = new_version.replace(".", "")
-    
-    _result = get_real_file(new_version, f"CheatEngine{version_short}.exe")
+        new_version_short = new_version.replace(".", "")
 
-    if not _result:
-        return
+        _result = get_real_file(new_version, f"CheatEngine{new_version_short}.exe", bar)
 
-    sha256_hash = subprocess.run(["utils\checksum.exe", "-t=sha256",
-                                 f"-f=tmp\CheatEngine{version_short}-real.exe"], stdout=subprocess.PIPE).stdout.decode("utf-8").split("\r")[0]
-    _result["checksum"] = sha256_hash
+        if not _result:
+            return
 
-    if DEBUG:
+        sha256_hash = subprocess.run(["utils\checksum.exe", "-t=sha256",
+                                      f"-f=tmp\CheatEngine{new_version_short}-real.exe"], stdout=subprocess.PIPE).stdout.decode("utf-8").split("\r")[0]
+        _result["checksum"] = sha256_hash
+
+        bar()
+        print("Printing the result...")
         print(_result)
-        time.sleep(2)
 
-    with open("tools\\chocolateyInstall.ps1", "r+", encoding="utf-8") as file:
-        lines = file.readlines()
-        for i in range(len(lines)):
-            # line = lines[i]
-            for j in range(len(REGEX_SCRIPT)-1):
-                _key = list(REGEX_SCRIPT.keys())[j]
-                lines[i] = re.sub(REGEX_SCRIPT[_key], REGEX_SCRIPT["sub"][_key].format(_result[_key]), lines[i])
+        with open("tools\\chocolateyInstall.ps1", "r+", encoding="utf-8") as file:
+            lines = file.readlines()
+            for i in range(len(lines)):
+                # line = lines[i]
+                for j in range(len(REGEX_SCRIPT)-1):
+                    _key = list(REGEX_SCRIPT.keys())[j]
+                    lines[i] = re.sub(REGEX_SCRIPT[_key], REGEX_SCRIPT["sub"][_key].format(_result[_key]), lines[i])
 
-        file.seek(0)
-        file.truncate()
-        file.writelines(lines)
+            file.seek(0)
+            file.truncate()
+            file.writelines(lines)
+
+        with open("tools\\chocolateyUninstall.ps1", "r+", encoding="utf-8") as file:
+            lines = file.readlines()
+            for i in range(len(lines)):
+                lines[i] = re.sub(r"\d\.\d", new_version, lines[i])
+
+            file.seek(0)
+            file.truncate()
+            file.writelines(lines)
+
+def check_for_updates():
+    releases = requests.get("https://api.github.com/repos/cheat-engine/cheat-engine/releases").json()
+    assets = [release["assets"] for release in releases if release["assets"] != []]
+    versions = [asset[0]["name"].split(".")[0].replace("CheatEngine", "") for asset in assets]    
+    versions = [f"{version[0]}.{version[1]}" for version in versions]
+    versions = versions[::-1]
+    print(versions)
+    return versions
 
 
 def test():
@@ -145,13 +166,13 @@ def test():
 
 
 if __name__ == "__main__":
-    VERSIONS = ["7.3", "7.4", "7.5"]
-    
+    VERSIONS = check_for_updates()
+
     if not os.path.exists("tmp"):
         os.mkdir("tmp")
     # test()
     for _version in VERSIONS:
-        main(_version, True)
+        main(_version)
         subprocess.run(["choco", "pack"])
         subprocess.run(["choco", "push", f"cheatengine.{_version}.nupkg"])
     shutil.rmtree("tmp")
